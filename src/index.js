25c28b24652d560d15c1a50697ad97fe38cb3c0a
GordonOtieno <otienogordon95@gmail.com>
2022-08-23 22:38:39 +0300

    set up project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import generatejoke from './generatejoke';
+import './styles/main.scss';
+import laughing from './assets/test.svg';
+
+const laughImg = document.getElementById('testImg');
+laughImg.src = laughing;
+
+const jokeBtn = document.getElementById('jokeBtn');
+jokeBtn.addEventListener('click', generatejoke);
+
+generatejoke();
